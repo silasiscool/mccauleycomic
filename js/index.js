b7af7c6f2e32ef2e958bfcebe47e4ee9e486c437
Silas Bartol <silasbartol@gmail.com>
2026-04-30 10:23:19 -0400

    Implemeted home page, data functions, and minor fixes

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// Get DOM Elements
+const comicElementIDs = ['comic1', 'comic2', 'comic3'];
+const comicElements = comicElementIDs.map(id=>document.getElementById(id));
+
+
+// Update images
+async function updateImages() {
+    // Get comics
+    const comics = await dateSortedComics();
+
+    // Find 3 most recent comics
+    const recentComics = comics.slice(0,comicElements.length)
+
+    // Assign images to comics
+    for (let i = 0; i < comicElements.length; i++) {
+        const comicElement = comicElements[i];
+        const comicData = recentComics[i];
+
+        // Apply src and alt
+        comicElement.src = R2RetriveImage(comicData.id);
+        comicElement.alt = "McCauley Comic: " + comicData.title;
+    }
+    
+}
+updateImages();
---
+++ js/master.js
@@ -0,0 +1,38 @@
+// Get query params
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+console.log()
+
+// Constants
+const metadataPath = '/data/comic_metadata.json';
+const R2BaseURL = 'https://pub-462b7d3f2c6946928c075e6c0fc28b67.r2.dev'
+
+// Function to get local metadata file
+async function getMetadata() {
+    const res = await fetch(metadataPath);
+    const metadata = res.json();
+    return metadata
+}
+
+// Return comics sorted by date
+async function dateSortedComics() {
+    // Get comic metadata
+    const metadata = await getMetadata();
+
+    // Get sorted list of comics
+    const comics = metadata.comics.sort((a,b) => {
+        if (b.date !== a.date) { // If dates are different, sort them
+            return b.date.localeCompare(a.date);
+        }
+        return 0; // If dates are identical, preserve order
+    })
+
+    return comics
+}
+
+// Get SRC for R2 image
+function R2RetriveImage(id) {
+    if (params.has('noloadimg'))
+        return '/media/placeholdercomic.png'
+    return `${R2BaseURL}/${id}.webp`
+}
